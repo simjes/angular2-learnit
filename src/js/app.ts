882696afe410c35c0f6f8454a2409b255d895cbe
Simon Jespersen <s.jespersen@stud.uis.no>
2015-10-31 16:00:05 +0100

    rdy for btn

--- src/js/app.ts
+++ src/js/app.ts
@@ -14,19 +14,14 @@ import {Contact} from './contact'
 @View({
     template: `
     <nav class="navbar navbar-fixed-top navbar-material-teal shadow-z-1 center">
-    <a class="navbar-brand" href="#">SIMJES</a>
-        <!-- <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <div class="navbar-collapse collapse navbar-responsive-collapse navbar-inner">
-            <ul class="nav navbar-nav">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">SIMJES</a>
+            <!--<ul class="nav navbar-nav">
                 <li><a [router-link]="['/Home']">HOME</a></li>
                 <li><a [router-link]="['/Portfolio']">PORTFOLIO</a></li>
                 <li><a [router-link]="['/Contact']">CONTACT</a></li>
-            </ul>
-        </div> -->
+            </ul>-->
+        </div>
     </nav>
     <router-outlet></router-outlet>
     <footer class="navbar navbar-fixed-bottom">
