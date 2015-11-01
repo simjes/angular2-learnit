83b63b9a12dcacd3244372b475fed6adf7588a6e
Simon Jespersen <s.jespersen@stud.uis.no>
2015-11-01 19:31:35 +0100

    jquery stuff

--- src/js/portfolio.ts
+++ src/js/portfolio.ts
@@ -1,7 +1,9 @@
 /// <reference path="../../node_modules/angular2/angular2.d.ts" />
 /// <reference path="../../node_modules/angular2/http.d.ts" />
-import {Component, View, NgFor,  NgIf} from 'angular2/angular2';
+import {Component, View, NgFor, NgIf} from 'angular2/angular2';
 import {Http, HTTP_PROVIDERS} from 'angular2/http';
+
+declare var jQuery: any;
 
 @Component({
     selector: 'portfolio',
@@ -15,11 +17,10 @@ import {Http, HTTP_PROVIDERS} from 'angular2/http';
 export class Portfolio {
     projects: Array<Object>;
 
-    constructor(http: Http) {
+    constructor( http: Http) {
         this.projects = [];
         http.get('projects.json')
             .map(res => res.json())
             .subscribe(projects => this.projects = projects);
-            
     }
 }
